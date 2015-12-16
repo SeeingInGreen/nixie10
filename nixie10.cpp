4080139fb8708f253d19a086d53260dd0c72fa2d
SeeingInGreen <chris.green63@gmail.com>
2015-12-15 21:09:19 -0500

    Bug fixes

--- nixie10.cpp
+++ nixie10.cpp
@@ -10,6 +10,7 @@ void nixie10::initialize_sn74141(int pins[4], int initVal){
 	_pins[1] = pins[1];
 	_pins[2] = pins[2];
 	_pins[3] = pins[3];
+	for(int i = 0; i < 4; i++) pinMode(_pins[i], OUTPUT);
 	set_sn74141(initVal);
 }
 
@@ -18,6 +19,7 @@ void nixie10::initialize_sn74141(int pins[4]){
 	_pins[1] = pins[1];
 	_pins[2] = pins[2];
 	_pins[3] = pins[3];
+	for(int i = 0; i < 4; i++) pinMode(_pins[i], OUTPUT);
 	set_sn74141(0);
 }
 
@@ -35,16 +37,16 @@ void nixie10::clear_sn74141(){
 void nixie10::_dec_to_bcd(int decimal, int* bcd){ //converts a decimal to a bcd string
 	//TODO: implement a safety feature
 	switch (decimal){
-		case 0: {bcd[3] = 0; bcd[2] = 0; bcd[1] = 0; bcd[0] = 0;}
-		case 1:	{bcd[3] = 0; bcd[2] = 0; bcd[1] = 0; bcd[0] = 1;}
-		case 2: {bcd[3] = 0; bcd[2] = 0; bcd[1] = 1; bcd[0] = 0;}
-		case 3: {bcd[3] = 0; bcd[2] = 0; bcd[1] = 1; bcd[0] = 1;}
-		case 4: {bcd[3] = 0; bcd[2] = 1; bcd[1] = 0; bcd[0] = 0;}
-		case 5: {bcd[3] = 0; bcd[2] = 1; bcd[1] = 0; bcd[0] = 1;}
-		case 6: {bcd[3] = 0; bcd[2] = 1; bcd[1] = 1; bcd[0] = 0;}
-		case 7: {bcd[3] = 0; bcd[2] = 1; bcd[1] = 1; bcd[0] = 1;}
-		case 8: {bcd[3] = 1; bcd[2] = 0; bcd[1] = 0; bcd[0] = 0;}
-		case 9: {bcd[3] = 1; bcd[2] = 0; bcd[1] = 0; bcd[0] = 1;}
-		default: {bcd[3] = 0; bcd[2] = 0; bcd[1] = 0; bcd[0] = 0;}
+		case 0 : {bcd[3] = 0; bcd[2] = 0; bcd[1] = 0; bcd[0] = 0; break;}
+		case 1 :	{bcd[3] = 0; bcd[2] = 0; bcd[1] = 0; bcd[0] = 1;break;}
+		case 2 : {bcd[3] = 0; bcd[2] = 0; bcd[1] = 1; bcd[0] = 0;break;}
+		case 3 : {bcd[3] = 0; bcd[2] = 0; bcd[1] = 1; bcd[0] = 1;break;}
+		case 4 : {bcd[3] = 0; bcd[2] = 1; bcd[1] = 0; bcd[0] = 0;break;}
+		case 5 : {bcd[3] = 0; bcd[2] = 1; bcd[1] = 0; bcd[0] = 1;break;}
+		case 6 : {bcd[3] = 0; bcd[2] = 1; bcd[1] = 1; bcd[0] = 0;break;}
+		case 7 : {bcd[3] = 0; bcd[2] = 1; bcd[1] = 1; bcd[0] = 1;break;}
+		case 8 : {bcd[3] = 1; bcd[2] = 0; bcd[1] = 0; bcd[0] = 0;break;}
+		case 9 : {bcd[3] = 1; bcd[2] = 0; bcd[1] = 0; bcd[0] = 1;break;}
+		default: {bcd[3] = 1; bcd[2] = 1; bcd[1] = 1; bcd[0] = 1;break;}
 	}
 }
