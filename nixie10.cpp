9bd440abb450316f02af1f0335952d7f524bcc8c
SeeingInGreen <chris.green63@gmail.com>
2016-01-09 22:27:56 -0500

    Added support for a 16-bit shift register mode (untested)

--- nixie10.cpp
+++ nixie10.cpp
@@ -1,6 +1,6 @@
 /* nixie10.cpp
 * A library for driving 10-number nixie tubes with Arduino and various circuit configurations
-* Chris Green - 11/29/2015
+* Chris Green - 1/9/2016
 */
 
 #include <nixie10.h>
@@ -37,8 +37,8 @@ void nixie10::clear_sn74141(){
 void nixie10::_dec_to_bcd(int decimal, int* bcd){ //converts a decimal to a bcd string
 	//TODO: implement a safety feature
 	switch (decimal){
-		case 0 : {bcd[3] = 0; bcd[2] = 0; bcd[1] = 0; bcd[0] = 0; break;}
-		case 1 :	{bcd[3] = 0; bcd[2] = 0; bcd[1] = 0; bcd[0] = 1;break;}
+		case 0 : {bcd[3] = 0; bcd[2] = 0; bcd[1] = 0; bcd[0] = 0;break;}
+		case 1 : {bcd[3] = 0; bcd[2] = 0; bcd[1] = 0; bcd[0] = 1;break;}
 		case 2 : {bcd[3] = 0; bcd[2] = 0; bcd[1] = 1; bcd[0] = 0;break;}
 		case 3 : {bcd[3] = 0; bcd[2] = 0; bcd[1] = 1; bcd[0] = 1;break;}
 		case 4 : {bcd[3] = 0; bcd[2] = 1; bcd[1] = 0; bcd[0] = 0;break;}
@@ -49,4 +49,34 @@ void nixie10::_dec_to_bcd(int decimal, int* bcd){ //converts a decimal to a bcd 
 		case 9 : {bcd[3] = 1; bcd[2] = 0; bcd[1] = 0; bcd[0] = 1;break;}
 		default: {bcd[3] = 1; bcd[2] = 1; bcd[1] = 1; bcd[0] = 1;break;}
 	}
+}
+
+void nixie10::initialize_16reg(int latchPin, int clockPin, int dataPin){ //these are pins connecting the 16-bit register to Arduino
+	_latchPin = latchPin;
+	_clockPin = clockPin;
+	_dataPin = dataPin;
+	pinMode(_latchPin, OUTPUT);
+	pinMode(_clockPin, OUTPUT);
+	pinMode(_dataPin, OUTPUT);
+	set_16reg(0, 0, 0, 0); //initialize register to 0'b0000000000000000
+}
+void nixie10::set_16reg(int ten_hour, int one_hour, int ten_minute, int one_minute){ //set all tube values
+	int ten_hour_a[4], one_hour_a[4], ten_minute_a[4], one_minute_a[4];
+	//create BCD arrays to shift in
+	_dec_to_bcd(ten_hour, ten_hour_a);
+	_dec_to_bcd(one_hour, one_hour_a);
+	_dec_to_bcd(ten_minute, ten_minute_a);
+	_dec_to_bcd(one_minute, one_minute_a);
+	//shift in arrays in most-to-lease significant bit order
+	//TODO: find a more efficient way to do this
+	for(int i = 3; i > 0; i-=1) _my_shift_out(_dataPin, _clockPin, ten_hour[i]);
+	for(int j = 3; j > 0; j-=1) _my_shift_out(_dataPin, _clockPin, one_hour[j]);
+	for(int k = 3; k > 0; k-=1) _my_shift_out(_dataPin, _clockPin, ten_minute[k]);
+	for(int l = 3; l > 0; l-=1) _my_shift_out(_dataPin, _clockPin, one_minute[l]);
+}
+void nixie10::_my_shift_out(int dataPin, int clockPin, int val){  //my own implementation of Arduino's shiftOut(), 1 bit only;
+	//TODO: check timing
+	digitalWrite(dataPin, !!(val)); //the double negation ensures one or zero
+	digitalWrite(clockPin, HIGH);
+    digitalWrite(clockPin, LOW);
 }
--- nixie10.h
+++ nixie10.h
@@ -1,6 +1,6 @@
 /* nixie10.h
 * A library for driving 10-number nixie tubes with Arduino and various circuit configurations
-* Chris Green - 11/29/2015
+* Chris Green - 1/9/2016
 */
 
 #ifndef nixie10_h
@@ -15,10 +15,15 @@ public:
 	void initialize_sn74141(int pins[4]); //initializes tube to zero
 	void clear_sn74141(); //makes the tube display nothing
 	void set_sn74141(int digit); //sets the tube to a 1-digit number
+	//initializers for 16-bit shift-register -> 4x sn74141 driver
+	void initialize_16reg(int latchPin, int clockPin, int dataPin); //these are pins connecting the 16-bit register to Arduino
+	void set_16reg(int ten_hour, int one_hour, int ten_minute, int one_minute); //set all tube values
 private:
+	void _my_shift_out(int dataPin, int clockPin, int val); //my own implementation of Arduino's shiftOut(), 1 bit only;
 	void _dec_to_bcd(int decimal, int *bcd); //converts a decimal to a bcd string
 	char _bcd[4]; //holds the current bcd output
 	char _pins[4]; //holds the 4 Arduino digital pins to drive the tube
+	int _latchPin, _clockPin, _dataPin; //pinout for a shift register configuration
 };
 
 #endif
